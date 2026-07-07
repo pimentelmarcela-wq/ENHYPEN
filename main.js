0fe8ac0fceffaf05a370d33943e2dc9855a671e0
pimentelmarcela-wq <pimentel.marcela@escola.pr.gov.br>
2026-07-07 10:29:07 -0300

    34

--- main.js
+++ main.js
@@ -4,5 +4,7 @@ const botoes = document.querySelectorAll("button")
 
             botoes.forEach(function (botao){
 let curtiu =false;
+botao.addEventListener("click", botaoClicado)
+
 
             })
