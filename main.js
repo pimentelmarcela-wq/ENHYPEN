322e61884bf914f5f2aced3327b85862a57f906d
pimentelmarcela-wq <pimentel.marcela@escola.pr.gov.br>
2026-07-07 10:35:49 -0300

    gt

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ let curtiu =false;
 botao.addEventListener("click", botaoClicado)
 function botaoClicado(){
     console.log("botao clicado")
+let texto = botao.querySelector("span")
+texto.textContent++
+
 }
 
 
