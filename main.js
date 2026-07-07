31f4a4e2be03a801d6ce4b87a7cea93f87c6b5fd
pimentelmarcela-wq <pimentel.marcela@escola.pr.gov.br>
2026-07-07 10:25:59 -0300

    s

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 alert("Hey, ENGENE! 🪐 > Você acabou de entrar no portal definitivo sobre o ENHYPEN. Prepare-se para conhecer curiosidades incríveis sobre os membros e o conceito de vampiros do grupo. Boa navegação!")
 const botoes = document.querySelectorAll("button")
     console.log(botoes)
+
+            botoes.forEach(function (botao){
+let curtiu =false;
+
+            })
