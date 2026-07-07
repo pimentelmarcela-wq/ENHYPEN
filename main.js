076e3771ad19a6d71145f910daa7f8a0c56a3276
pimentelmarcela-wq <pimentel.marcela@escola.pr.gov.br>
2026-07-07 10:13:05 -0300

    bgh

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-alert("olá mundo")
+alert("Hey, ENGENE! 🪐 > Você acabou de entrar no portal definitivo sobre o ENHYPEN. Prepare-se para conhecer curiosidades incríveis sobre os membros e o conceito de vampiros do grupo. Boa navegação!")
